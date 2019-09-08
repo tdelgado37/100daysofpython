0e8b5497e8e05880964bf30f1270aebde4a84e17
tdelgado37 <tdelgado37.td@gmail.com>
2019-09-08 10:48:55 -0400

    added logic to file to represent looting and adding inventory

--- fantasy_game_inventory.py
+++ fantasy_game_inventory.py
@@ -1,2 +1,25 @@
-inventory = { 'rope':1, 'torch': 6, 'gold coin': 42, 'dagger': 1,
-}
+import pprint
+
+inventory = { 'rope':1, 'torch': 6, 'gold coin': 42, 'dagger': 1}
+
+
+
+def displayInventory(invtry):
+    pprint.pprint(invtry)
+    total_count = 0
+    for key in invtry:
+        total_count += invtry[key]
+    pprint.pprint('Total inventory count: ' + str(total_count))
+
+
+displayInventory(inventory)
+
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+def addToInventory(invtry, items):
+    for item in items:
+        invtry.setdefault(item, 0)
+        invtry[item] +=1
+    return invtry
+inventory = addToInventory(inventory, dragonLoot)
+displayInventory(inventory)
