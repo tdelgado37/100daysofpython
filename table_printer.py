b41ef982fab987c77fbac553c6bd0acb8b8ad509
tdelgado37 <tdelgado37.td@gmail.com>
2019-09-10 19:17:45 -0400

    created a table_printer program to practice right-justifed manipulation, also to practice 2D list taverse

---
+++ table_printer.py
@@ -0,0 +1,28 @@
+#! python3
+# table_printer.py - prints a list of lists of strings in
+# a well-organized table with each column right-justified.
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+    ['Alice', 'Bob', 'Carol', 'David'],
+    ['dogs', 'cats', 'moose', 'goose']
+ ]
+
+def printTable(tableData):
+    colWidths = [0] * len(tableData)
+    numRows = len(tableData[0])
+    lines = []
+    for column_num, column in enumerate(tableData):
+        for element in column:
+            if(len(element)>colWidths[column_num]):
+                colWidths[column_num] = len(element)
+
+
+    for row_index in range(numRows):
+        line = ''
+        for column_num, column in enumerate(tableData):
+            column_width = colWidths[column_num]
+            line += column[row_index].rjust(column_width) + ' '
+        print(line)
+
+
+
+printTable(tableData)
