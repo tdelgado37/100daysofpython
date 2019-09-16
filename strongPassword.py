ac3ae3667115a16548a747410cf5b2973990e5ca
tdelgado37 <tdelgado37.td@gmail.com>
2019-09-16 17:27:21 -0400

    created another python practice project for regex

---
+++ strongPassword.py
@@ -0,0 +1,14 @@
+#! python3
+#strongPassword.py - tells if the inputed string is a strong strongPassword
+
+import re
+
+password = input('Enter a strong password: ')
+
+strongPasswordRegex = re.compile(r'[A-Za-z0-9{1,}]{8,}')
+results = strongPasswordRegex.search(password)
+
+if results is None:
+    print('Not a strong password, password needs some gains!')
+else:
+    print('That password Arnold strong!')
